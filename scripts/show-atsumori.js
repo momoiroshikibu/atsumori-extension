8cc9aca720cd9576731e1b020590d3c8b9985f33
momoiroshikibu <momoiroshikibu@gmail.com>
2017-09-02 23:56:50 +0900

    v1.0.2

--- scripts/show-atsumori.js
+++ scripts/show-atsumori.js
@@ -11,6 +11,8 @@ const timestamp = (new Date()).getTime();
 
 if (isHot) {
     showAtsumori();
+    const soundFileName = (timestamp % 2 === 0)? 'atsumori.mp3' : 'aaatsumori.mp3';
+    (new Audio(chrome.extension.getURL('sound/' + soundFileName))).play();
 }
 
 function showAtsumori() {
@@ -27,5 +29,6 @@ function showAtsumori() {
     body.appendChild(image);
     image.onclick = (e) => {
         e.target.remove();
+        (new Audio(chrome.extension.getURL('sound/shitsurei.mp3'))).play();
     }
 }
