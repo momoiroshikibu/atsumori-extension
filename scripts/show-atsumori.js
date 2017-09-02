e8fc2158177d202415dab731722113cb255d0c29
momoiroshikibu <momoiroshikibu@gmail.com>
2017-09-02 23:55:45 +0900

    v1.0.1

--- scripts/show-atsumori.js
+++ scripts/show-atsumori.js
@@ -1,10 +1,31 @@
-const image = document.createElement('img');
-image.src = chrome.extension.getURL('images/atsumori.png');
-image.className = 'atsumori';
-image.style = `
+const hotWords = ['熱', '勝', '台風', 'サンシャイン池崎', '修造', 'アントニオ猪木']
+
+const body = document.getElementsByTagName('body')[0];
+const documentText = body.innerText;
+
+const isHot = hotWords.find((hotWord) => {
+    return (documentText.indexOf(hotWord) > -1)
+});
+
+const timestamp = (new Date()).getTime();
+
+if (isHot) {
+    showAtsumori();
+}
+
+function showAtsumori() {
+    const image = document.createElement('img');
+    image.src = chrome.extension.getURL('images/atsumori.png');
+    image.className = 'atsumori';
+    image.style = `
   position: fixed;
   right: 50px;
   bottom: 0px;
   z-index: 100000;
+  cursor: pointer;
 `;
-document.getElementsByTagName("body")[0].appendChild(image);
+    body.appendChild(image);
+    image.onclick = (e) => {
+        e.target.remove();
+    }
+}
